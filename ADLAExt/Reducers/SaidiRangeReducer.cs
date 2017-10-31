175dd2e7a9b86d06e815cb34228ec352f46d212f
cloud4yourdata <cloud4yourdata@outlook.com>
2017-10-31 10:04:14 +0100

    U-SQL Demos

---
+++ ADLAExt/Reducers/SaidiRangeReducer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ADLAExt.Reducers
+{
+    public class SaidiRangeReducer : IReducer
+    {
+        const string BeginColName = "begin";
+        const string EndColName = "end";
+        const string SaidiColName = "saidi";
+        private readonly int _maxDuration;
+        public SaidiRangeReducer(int maxDuration)
+        {
+            _maxDuration = maxDuration;
+        }
+        public override IEnumerable<IRow> Reduce(IRowset input, IUpdatableRow output)
+        {
+            // Init aggregation values
+            var firstRowProcessed = false;
+            var begin = DateTime.MinValue;
+            var end = DateTime.MinValue;
+            var saidivalue = 0.0;
+            // requires that the reducer is PRESORTED on begin and READONLY on the reduce key.
+            foreach (var row in input.Rows)
+            {
+                if (!firstRowProcessed)
+                {
+                    firstRowProcessed = true;
+                    begin = row.Get<DateTime>(BeginColName);
+                    end = row.Get<DateTime>(EndColName);
+                    saidivalue = row.Get<double>(SaidiColName);
+                }
+                else
+                {
+                    var b = row.Get<DateTime>("begin");
+                    var e = row.Get<DateTime>("end");
+                    var tmpsaidi = row.Get<double>("saidi");
+                    if ((b - end).TotalSeconds <= _maxDuration)
+                    {
+                        saidivalue += tmpsaidi;
+                    }
+                    else
+                    {
+                        output.Set<double>("saidi", saidivalue);
+                        output.Set<DateTime>("begin", begin);
+                        output.Set<DateTime>("end", end);
+
+                        yield return output.AsReadOnly();
+                        saidivalue = tmpsaidi;
+                        begin = b;
+                    }
+                    end = e;
+
+                }
+            }
+            output.Set<DateTime>("begin", begin);
+            output.Set<DateTime>("end", end);
+            output.Set<double>("saidi", saidivalue);
+            yield return output.AsReadOnly();
+        }
+    }
+}
---
+++ ADLAExt/Utils/VertextInfo.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+
+namespace ADLAExt.Utils
+{
+    public static class VertextInfo
+    {
+        public static string GetInfo()
+        {
+            var info = "";
+            var procQuery = "SELECT Name,NumberOfCores FROM Win32_Processor";
+            var pObjectSearchersearcher = new ManagementObjectSearcher(procQuery);
+            foreach (ManagementObject WniPART in pObjectSearchersearcher.Get())
+            {
+                info =
+                    $"Processor:{WniPART.Properties["Name"].Value};NumberOfCores:{WniPART.Properties["NumberOfCores"].Value};";
+            }
+
+            var Query = "SELECT Capacity FROM Win32_PhysicalMemory";
+            var searcher = new ManagementObjectSearcher(Query);
+            var totalMemorySizeInGb = (from ManagementObject wniPart in searcher.Get()
+                                       select Convert.ToUInt64(wniPart.Properties["Capacity"].Value)
+                into sizeinByte
+                                       select sizeinByte / 1024 / 1024 / 1024).Aggregate<ulong, ulong>(0, (current, sizeinGb) => current + sizeinGb);
+            info += $"RAM Size in GB: {totalMemorySizeInGb}";
+            return info;
+        }
+
+        public static string GetFullInfo()
+        {
+            var sb = new StringBuilder();
+            var myManagementClass = new
+                ManagementClass("Win32_OperatingSystem");
+            var myManagementCollection =
+                myManagementClass.GetInstances();
+            var myProperties =
+                myManagementClass.Properties;
+            var myPropertyResults =
+                new Dictionary<string, object>();
+
+            foreach (var obj in myManagementCollection)
+            {
+                foreach (var myProperty in myProperties)
+                {
+                    myPropertyResults.Add(myProperty.Name,
+                        obj.Properties[myProperty.Name].Value);
+                }
+            }
+
+            foreach (var myPropertyResult in myPropertyResults)
+            {
+                var item = $"{myPropertyResult.Key}:{myPropertyResult.Value}";
+                sb.AppendLine(item);
+            }
+            return sb.ToString();
+        }
+
+        public static string GetVMInfo()
+        {
+            var sb = new StringBuilder();
+            var myManagementClass = new
+                ManagementClass("Win32_PerfRawData_Counters_HyperVDynamicMemoryIntegrationService");
+            var myManagementCollection =
+                myManagementClass.GetInstances();
+            var myProperties =
+                myManagementClass.Properties;
+            var myPropertyResults =
+                new Dictionary<string, object>();
+
+            foreach (var obj in myManagementCollection)
+            {
+                foreach (var myProperty in myProperties)
+                {
+                    myPropertyResults.Add(myProperty.Name,
+                        obj.Properties[myProperty.Name].Value);
+                }
+            }
+
+            foreach (var myPropertyResult in myPropertyResults)
+            {
+                var item = $"{myPropertyResult.Key}:{myPropertyResult.Value}";
+                sb.AppendLine(item);
+            }
+            return sb.ToString();
+        }
+    }
+}
